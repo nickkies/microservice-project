fc9e8e6de9cfad6effb253c6fb3b12ae64dc1258
YoonSung Kim <opqudk@gmail.com>
2024-02-20 15:30:48 +0900

    ✨ feat: create user

--- src/auth-service/users.rs
+++ src/auth-service/users.rs
@@ -1,11 +1,19 @@
 use std::collections::HashMap;
 
+use pbkdf2::{
+    password_hash::{PasswordHasher, SaltString},
+    Pbkdf2,
+};
+use rand_core::OsRng;
+use uuid::Uuid;
+
 pub trait Users {
     fn create_user(&mut self, username: String, password: String) -> Result<(), String>;
     fn get_user_uuid(&self, username: String, password: String) -> Option<String>;
     fn delete_user(&mut self, user_uuid: String);
 }
 
+#[derive(Clone)]
 pub struct User {
     user_uuid: String,
     username: String,
@@ -20,7 +28,27 @@ pub struct UsersImpl {
 
 impl Users for UsersImpl {
     fn create_user(&mut self, username: String, password: String) -> Result<(), String> {
-        todo!();
+        if self.username_to_user.contains_key(&username) {
+            return Err("Username already exists. Unable to create user.".to_string());
+        }
+
+        let salt = SaltString::generate(&mut OsRng);
+
+        let hashed_password = Pbkdf2
+            .hash_password(password.as_bytes(), &salt)
+            .map_err(|e| format!("Failed to hash password.\n{e:?}"))?
+            .to_string();
+
+        let user = User {
+            user_uuid: Uuid::new_v4().to_string(),
+            username: username.clone(),
+            password: hashed_password,
+        };
+
+        self.username_to_user.insert(username, user.clone());
+        self.uuid_to_user.insert(user.user_uuid.clone(), user);
+
+        Ok(())
     }
 
     fn get_user_uuid(&self, username: String, password: String) -> Option<String> {
