3e4276b9346776c8a8378acac91681ccf4ba3eda
YoonSung Kim <opqudk@gmail.com>
2024-02-24 14:39:16 +0900

    ✨ feat: auth service main

--- src/auth-service/main.rs
+++ src/auth-service/main.rs
@@ -1,14 +1,27 @@
-use auth::AuthService;
+use std::sync::Mutex;
+
+use auth::{authentication::auth_server::AuthServer, AuthService};
+use sessions::SessionsImpl;
+use tonic::transport::Server;
+use users::UsersImpl;
 
 mod auth;
 mod sessions;
 mod users;
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let users_service = todo!();
-    let sessions_service = todo!();
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let addr = "[::0]:50051".parse()?;
+
+    let users_service = Box::new(Mutex::new(UsersImpl::default()));
+    let sessions_service = Box::new(Mutex::new(SessionsImpl::default()));
 
     let auth_service = AuthService::new(users_service, sessions_service);
+
+    Server::builder()
+        .add_service(AuthServer::new(auth_service))
+        .serve(addr)
+        .await?;
 
     Ok(())
 }
